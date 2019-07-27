95769540907f3e4df6fed1ede75d1d1be5a58960
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-07-27 12:05:54 +0200

    feat: Fetch the articles from Reddit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello world!
-    </div>
-  );
+class App extends Component {
+  state = {
+    articles: null,
+  }
+
+  componentDidMount() {
+    fetch('https://www.reddit.com/best.json')
+      .then(r => {
+        if (r.status === 200) {
+          r.json().then(json => this.setState({ articles: json.data.children }))
+        }
+      }
+    )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        Hello world!
+      </div>
+    );
+  }
 }
 
 export default App;
