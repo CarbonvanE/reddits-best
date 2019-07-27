4605fabf1c25740c3eca04827de9f55870750681
Ruben <25854734+CarbonvanE@users.noreply.github.com>
2019-07-27 14:36:22 +0200

    feat: Add a dot between the thousands of larger scores

--- src/list/ListItem.js
+++ src/list/ListItem.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './ListItem.scss';
 
 function ListItem(props) {
-  console.log(props.data);
+  const formatNumber = x => {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+  }
+
   return (
     <div className="ListItem">
       <div className="title">{props.data.title}</div>
       <div className="info">
       <div className="subreddit">r/{props.data.subreddit}</div>
-        <div className="points">
-          <span className="score">{props.data.score}</span>
-          <span>points</span>
-        </div>
+      <div className="spacer">·</div>
+      <div className="points">
+        <span className="score">{formatNumber(props.data.score)}</span>
+        <span>points</span>
+      </div>
       </div>
     </div>
   );
